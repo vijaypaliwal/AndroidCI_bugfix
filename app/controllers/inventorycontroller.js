39f5478f021a06a7df7b1f32509f064d5dcd4852
dashrathkumawat <dashrath.k@shivamitconsultancy.com>
2017-02-16 10:26:01 +0530

    minor change
    minor change

--- app/controllers/inventorycontroller.js
+++ app/controllers/inventorycontroller.js
@@ -2551,7 +2551,7 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
             targeHeight: 120,
             correctOrientation: true,
             destinationType: destinationType.DATA_URL,
-            allowEdit: true,
+            allowEdit: false,
             saveToPhotoAlbum: true,
         });
     }
