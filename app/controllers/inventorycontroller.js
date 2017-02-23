dee188e7789354986074de4cf3687cd3f590b4c0
dashrathkumawat <dashrath.k@shivamitconsultancy.com>
2017-02-23 12:05:36 +0530

    Image Save
    Image Save

--- app/controllers/inventorycontroller.js
+++ app/controllers/inventorycontroller.js
@@ -2558,6 +2558,17 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
     $scope.onPhotoDataSuccessNew = function (imageData) {
         var _ImgObj = { ImageID: 0, FileName: "", bytestring: "", Size: 0 }
 
+
+        var myBase64 = imageData;
+        // To define the type of the Blob
+        var contentType = "image/png";
+        // if cordova.file is not available use instead :
+        // var folderpath = "file:///storage/emulated/0/";
+        var folderpath = cordova.file.externalRootDirectory;
+        var filename = "ourcodeworld.png";
+
+       
+
         imageData = "data:image/jpeg;base64," + imageData;
 
         var id = randomStringNew(5, '0123456789');
@@ -2571,7 +2582,7 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
         _ImgObj.bytestring = imageData;
         $scope.ImageList.push(_ImgObj);
         CheckScopeBeforeApply();
-
+        savebase64AsImageFile(folderpath, filename, myBase64, contentType);
         // log.success("Images captured length"+$scope.ImageList.length);
 
     }
@@ -2584,7 +2595,56 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
         $("#camerapermission").modal("show");
     }
 
+    function b64toBlob(b64Data, contentType, sliceSize) {
+        contentType = contentType || '';
+        sliceSize = sliceSize || 512;
 
+        var byteCharacters = atob(b64Data);
+        var byteArrays = [];
+
+        for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+            var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+            var byteNumbers = new Array(slice.length);
+            for (var i = 0; i < slice.length; i++) {
+                byteNumbers[i] = slice.charCodeAt(i);
+            }
+
+            var byteArray = new Uint8Array(byteNumbers);
+
+            byteArrays.push(byteArray);
+        }
+
+        var blob = new Blob(byteArrays, { type: contentType });
+        return blob;
+    }
+
+    /**
+     * Create a Image file according to its database64 content only.
+     * 
+     * @param folderpath {String} The folder where the file will be created
+     * @param filename {String} The name of the file that will be created
+     * @param content {Base64 String} Important : The content can't contain the following string (data:image/png[or any other format];base64,). Only the base64 string is expected.
+     */
+    function savebase64AsImageFile(folderpath, filename, content, contentType) {
+        // Convert the base64 string in a Blob
+        var DataBlob = b64toBlob(content, contentType);
+
+        alert("Starting to write the file :3");
+
+        window.resolveLocalFileSystemURL(folderpath, function (dir) {
+           alert("Access to the directory granted succesfully");
+            dir.getFile(filename, { create: true }, function (file) {
+                alert("File created succesfully.");
+                file.createWriter(function (fileWriter) {
+                    alert("Writing content to file");
+                    fileWriter.write(DataBlob);
+                }, function () {
+                    alert('Unable to save file in path ' + folderpath);
+                });
+            });
+        });
+    }
   
     $scope.capturePhotoNew = function () {
 
@@ -2592,11 +2652,11 @@ app.controller('inventoryController', ['$scope', '$location', 'authService', 'lo
         navigator.camera.getPicture($scope.onPhotoDataSuccessNew, $scope.onFail, {
             quality: 50,
             targetWidth: 120,
-            targeHeight: 120,
+            targetHeight: 120,
             correctOrientation: true,
             destinationType: destinationType.DATA_URL,
             allowEdit: false,
-            saveToPhotoAlbum: true,
+            saveToPhotoAlbum: false,
         });
     }
 
