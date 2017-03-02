0e5c9f74f387199ac6b36c1722a210a7e85370cc
ajay <ajaypaliwal.cto@outlook.com>
2017-03-02 16:46:21 +0530

    test
    test

--- app/controllers/profileController.js
+++ app/controllers/profileController.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-app.controller('profileController', ['$scope',  'localStorageService', 'authService', '$location', 'log', function ($scope,  localStorageService, authService, $location, log) {
+app.controller('profileController', ['$scope', 'localStorageService', 'authService', '$location', 'log', function ($scope, localStorageService, authService, $location, log) {
     $scope.Currentuser = {};
     $scope.SavingData = false;
     $scope.IsEditMode = false;
@@ -11,7 +11,7 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
     $scope.organization = "";
     $scope.email = "";
     $scope.picURl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
-    $scope.Image={ImageID:0,FileName:"",bytestring:"",Size:0}
+    $scope.Image = { ImageID: 0, FileName: "", bytestring: "", Size: 0 }
     $scope.isSaving = false;
     $scope._isProfileLoading = false;
     function init() {
@@ -36,9 +36,18 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
         return bytes;
     }
 
+    $scope.IsdummyImage = true;
+
 
     $scope.PreviewImage = function () {
-        $("#imagemodaldetail").modal("show");
+
+
+
+        if ($scope.IsdummyImage == false) {
+            $("#imagemodaldetail").modal("show");
+        }
+
+
     }
 
     $scope.Getuserinfo = function () {
@@ -63,62 +72,59 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
 
                    debugger;
                    if (response.GetUserInfoResult.Success == true) {
-                       
-                       
+
+
                        $scope.firstname = response.GetUserInfoResult.Payload[0].FirstName
-                   $scope.lastname = response.GetUserInfoResult.Payload[0].LastName;
-                   $scope.email = response.GetUserInfoResult.Payload[0].Email
-                   $scope.phone = response.GetUserInfoResult.Payload[0].Phone
-                   $scope.organization = response.GetUserInfoResult.Payload[0].Organization
-                   $scope.username = response.GetUserInfoResult.Payload[0].UserName
-                   $scope.myprofileimage = response.GetUserInfoResult.Payload[0].ProfilePic;
+                       $scope.lastname = response.GetUserInfoResult.Payload[0].LastName;
+                       $scope.email = response.GetUserInfoResult.Payload[0].Email
+                       $scope.phone = response.GetUserInfoResult.Payload[0].Phone
+                       $scope.organization = response.GetUserInfoResult.Payload[0].Organization
+                       $scope.username = response.GetUserInfoResult.Payload[0].UserName
+                       $scope.myprofileimage = response.GetUserInfoResult.Payload[0].ProfilePic;
 
 
-                   $scope.firstnameLabel = response.GetUserInfoResult.Payload[0].FirstName
-                   $scope.lastnameLabel = response.GetUserInfoResult.Payload[0].LastName;
-                   $scope.emailLabel = response.GetUserInfoResult.Payload[0].Email
-                   $scope.phoneLabel = response.GetUserInfoResult.Payload[0].Phone
-                   $scope.organizationLabel = response.GetUserInfoResult.Payload[0].Organization
-                   $scope.usernameLabel = response.GetUserInfoResult.Payload[0].UserName
+                       $scope.firstnameLabel = response.GetUserInfoResult.Payload[0].FirstName
+                       $scope.lastnameLabel = response.GetUserInfoResult.Payload[0].LastName;
+                       $scope.emailLabel = response.GetUserInfoResult.Payload[0].Email
+                       $scope.phoneLabel = response.GetUserInfoResult.Payload[0].Phone
+                       $scope.organizationLabel = response.GetUserInfoResult.Payload[0].Organization
+                       $scope.usernameLabel = response.GetUserInfoResult.Payload[0].UserName
 
-                   if (response.GetUserInfoResult.Payload[0].ProfilePic != null && response.GetUserInfoResult.Payload[0].ProfilePic != "") {
+                       if (response.GetUserInfoResult.Payload[0].ProfilePic != null && response.GetUserInfoResult.Payload[0].ProfilePic != "") {
 
 
-                       if (response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("png") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("jpg") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("jpeg") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("gif") != -1) {
-                           $scope.picURl = response.GetUserInfoResult.Payload[0].ProfilePic;
+                           if (response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("png") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("jpg") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("jpeg") != -1 || response.GetUserInfoResult.Payload[0].ProfilePic.indexOf("gif") != -1) {
+                               $scope.picURl = response.GetUserInfoResult.Payload[0].ProfilePic;
+                               $scope.IsdummyImage = false;
 
-                           $scope.ProfilePicURl = $scope.picURl;
+                               $scope.ProfilePicURl = $scope.picURl;
+                           }
+
+                           else {
+                               $scope.picURl = "img/No_image_available.svg";
+                               $scope.IsdummyImage = true;
+                               $scope.ProfilePicURl = "img/dummy-user48.png";
+
+                           }
+
                        }
 
                        else {
+
                            $scope.picURl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
 
                            $scope.ProfilePicURl = "img/dummy-user48.png";
 
-
                        }
 
-
-                      
-                    
-                   }
-
-                   else {
-
-                       $scope.picURl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg";
-
-                       $scope.ProfilePicURl = "img/dummy-user48.png";
-
-                   }
-                   
-                   $scope._isProfileLoading = false;
+                       $scope._isProfileLoading = false;
                    }
                    else {
                        $scope.ShowErrorMessage("User Info", 1, 1, response.GetUserInfoResult.Message)
 
                    }
 
-                 
+
 
 
                    $scope.$apply();
@@ -127,8 +133,9 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
                    $("#myimg").attr("src", $scope.picURl + "?" + d.getTime());
                    $("#myimgProfile").attr("src", $scope.ProfilePicURl + "?" + d.getTime());
                    $("#myimgProfile1").attr("src", $scope.ProfilePicURl + "?" + d.getTime());
+
                    $("#imagepreview").attr("src", $scope.picURl + "?" + d.getTime());
-                   
+
                },
                error: function (err) {
 
@@ -149,14 +156,14 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
     });
 
 
- 
+
 
     $scope.handleFileSelect = function (evt) {
 
 
         var files = evt.target.files;
-      var  FileName = "";
-      var  StreamData = "";
+        var FileName = "";
+        var StreamData = "";
         var _ImgObj = { ImageID: 0, FileName: "", bytestring: "", Size: 0 }
         // Loop through the FileList and render image files as thumbnails.
         for (var i = 0, f; f = files[i]; i++) {
@@ -204,7 +211,7 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
     $scope.openModel = function () {
         $("#myModalforlist").modal("show");
 
-     //   $("#files").trigger("click");
+        //   $("#files").trigger("click");
     }
 
     $scope.Openbottommenu = function () {
@@ -265,7 +272,7 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
 
         _ImgObj.FileName = "IphoneCapture";
         _ImgObj.bytestring = imageData;
-        $scope.Image=_ImgObj;
+        $scope.Image = _ImgObj;
         CheckScopeBeforeApply();
         $scope.uploadProfile();
 
@@ -309,7 +316,6 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
 
                        $scope.Getuserinfo();
                        ShowSuccess("Updated");
-
                    }
                    else {
                        $scope.ShowErrorMessage("Upload profile image", 1, 1, response.UploadProfileImageResult.Message)
@@ -329,7 +335,7 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
 
     $scope.Updateinfo = function () {
 
-         
+
         var authData = localStorageService.get('authorizationData');
         if (authData) {
             $scope.SecurityToken = authData.token;
@@ -338,32 +344,32 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
         var _data = { "UserName": $scope.username, "FirstName": $scope.firstname, "LastName": $scope.lastname, "Email": $scope.email, "Phone": $scope.phone, "Organization": $scope.organization, "ProfilePic": $scope.myprofileimage };
 
         $scope.isSaving = true;
-         
+
         $.ajax({
             url: serviceBase + "UpdateUserInfo",
             type: 'POST',
-            data: JSON.stringify({ "SecurityToken": $scope.SecurityToken, "data": _data,"IsUserInfo":true }),
+            data: JSON.stringify({ "SecurityToken": $scope.SecurityToken, "data": _data, "IsUserInfo": true }),
             dataType: 'json',
             contentType: 'application/json',
             success: function (result) {
                 // log.success("Profile information Updated.");
 
                 if (result.UpdateUserInfoResult.Success == true) {
-               
-                ShowSuccess("Updated");
 
-                $scope.firstnameLabel = $scope.firstname;
-                $scope.lastnameLabel = $scope.lastname;
-                $scope.emailLabel = $scope.email;
-                $scope.phoneLabel = $scope.phone;
-                $scope.organizationLabel = $scope.organization;
-                $scope.usernameLabel = $scope.username;
+                    ShowSuccess("Updated");
+
+                    $scope.firstnameLabel = $scope.firstname;
+                    $scope.lastnameLabel = $scope.lastname;
+                    $scope.emailLabel = $scope.email;
+                    $scope.phoneLabel = $scope.phone;
+                    $scope.organizationLabel = $scope.organization;
+                    $scope.usernameLabel = $scope.username;
 
 
-                $(".detailmode").show();
-                $(".editmode").hide();
-                $scope.isSaving = true;
-                $scope.$apply();
+                    $(".detailmode").show();
+                    $(".editmode").hide();
+                    $scope.isSaving = true;
+                    $scope.$apply();
 
                 }
                 else {
@@ -378,20 +384,19 @@ app.controller('profileController', ['$scope',  'localStorageService', 'authServ
 
                 $scope.isSaving = false;
                 $scope.$apply();
-            
+
             },
             complete: function () {
                 $scope.isSaving = false;
                 $scope.$apply();
             }
         });
-    
+
     }
 
 
 
-    $scope.logOut = function ()
-    {
+    $scope.logOut = function () {
         authService.logOut();
         $location.path('/login');
         CheckScopeBeforeApply();
