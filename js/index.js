5b64ffe0f6a158b0ba89af5184c49462a5215937
gautam <gautam.p@shivamitconsultancy.com>
2018-07-24 13:24:11 +0530

    Splash screen...
    Splash screen...

--- js/index.js
+++ js/index.js
@@ -498,6 +498,10 @@ function UpdateStatusBar(Type) {
 
 function onDeviceReady() {
 
+    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType;
+
     window.plugins.NativeAudio.preloadSimple('click', 'audio/click.mp3', function (msg) {
     }, function (msg) {
         //alert('error: ' + msg);
@@ -507,13 +511,6 @@ function onDeviceReady() {
     }, function (msg) {
         //alert('error: ' + msg);
     });
-
-
-
-    var deviceType = (navigator.userAgent.match(/iPad/i)) == "iPad" ? "iPad" : (navigator.userAgent.match(/iPhone/i)) == "iPhone" ? "iPhone" : (navigator.userAgent.match(/Android/i)) == "Android" ? "Android" : (navigator.userAgent.match(/BlackBerry/i)) == "BlackBerry" ? "BlackBerry" : "null";
-    pictureSource = navigator.camera.PictureSourceType;
-    destinationType = navigator.camera.DestinationType;    
-   
 
     //InitializeModal();
 
